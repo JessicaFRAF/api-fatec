ed74e6812af1f1a01a654fe709deb9a314732ba2
JessicaFRAF <jessicaamaral_rolim@hotmail.com>
2024-02-29 22:44:56 -0300

    aula 29-02

--- src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
+++ src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
@@ -1,10 +1,33 @@
 package br.com.api.fatec.apifatec;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController 
 @SpringBootApplication
 public class ApiFatecApplication {
+	
+	@RequestMapping("/")
+	String home () {
+		return "Hello Mundo!!";
+	}
+	@RequestMapping("/hello")
+	String home2 () {
+		return "Hello World 2";
+	}
+	
+	@RequestMapping("/numero")
+	Integer numero () {
+		return 11;
+	}
+	
+	@RequestMapping ("/numero/{num}")
+	Integer numero2 (@PathVariable Integer num) {
+		return num;
+		}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFatecApplication.class, args);
